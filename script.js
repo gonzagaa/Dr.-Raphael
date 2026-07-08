383be0d4b0a38b84f25da98986fa2dc957a258fd
gonzxgx <iigust4vo@gmail.com>
2026-07-08 10:41:39 -0300

    update

--- script.js
+++ script.js
@@ -10,43 +10,62 @@ function raf(time) {
 }
 requestAnimationFrame(raf);
 
-AOS.init({
-  duration: 1200,
-});
+// Os carrosséis ficam abaixo da dobra: inicializar o Swiper apenas quando
+// o usuário se aproximar deles tira esse custo do carregamento inicial.
+function iniciarSwipers() {
+  const larguraDaTela = window.innerWidth;
 
-const larguraDaTela = window.innerWidth;
+  if (larguraDaTela < 800) {
+    new Swiper(".mySwiper3", {
+      grabCursor: true,
+      effect: "creative",
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+      creativeEffect: {
+        prev: {
+          shadow: true,
+          translate: ["-120%", 0, -1],
+        },
+        next: {
+          translate: ["110%", 0, 0],
+        },
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  } else {
+    new Swiper(".mySwiper3", {
+      slidesPerView: 4,
+      spaceBetween: 10,
+      loop: true,
+      grabCursor: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
+}
 
-if (larguraDaTela < 800) {
-  var swiper3 = new Swiper(".mySwiper3", {
-    grabCursor: true,
-    effect: "creative",
-    autoplay: {
-      delay: 3500,
-      disableOnInteraction: false,
-    },
-    creativeEffect: {
-      prev: {
-        shadow: true,
-        translate: ["-120%", 0, -1],
+const carrosseis = document.querySelectorAll(".mySwiper3");
+
+if (carrosseis.length > 0) {
+  if ("IntersectionObserver" in window) {
+    const observador = new IntersectionObserver(
+      (entradas, obs) => {
+        if (entradas.some((e) => e.isIntersecting)) {
+          obs.disconnect();
+          iniciarSwipers();
+        }
       },
-      next: {
-        translate: ["110%", 0, 0],
-      },
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-} else {
-  var swiper3 = new Swiper(".mySwiper3", {
-    slidesPerView: 4,
-    spaceBetween: 10,
-    loop: true,
-    grabCursor: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+      { rootMargin: "800px 0px" }
+    );
+    carrosseis.forEach((el) => observador.observe(el));
+  } else {
+    iniciarSwipers();
+  }
 }
